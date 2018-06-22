130f287b69d9dad89cf28779b34741b648a33775
adebayo ajagunna <adebayoajagunna@gmail.com>
2018-06-22 15:53:36 -0400

    Add files via upload
    This is a weatherapp  application that uses an API to receive the current weather of any city in the world.
    You will need an API key in other to run this application. 
    You can sign up for one at www.openweathermap.org.
    
    Please installl npm yargs into a folder. This is an Asynchronous Programming.
    Uses can run with command node app.js -l {city goes here}

---
+++ Day19/weather.js
@@ -0,0 +1,39 @@
+var request = require('request');
+
+
+
+
+
+
+module.exports = function (location, callback){
+    var encodedLocation = encodeURIComponent(location)
+
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q='+ encodedLocation + '&units=imperial&appid=9afb2bc96d81f641d1ea875ce223f976'
+
+
+    if (!location){
+
+        return callback('No location Provided')
+    }
+
+            
+        request({
+
+            url:url,
+            json:true
+        }, function(error,response,body){
+            if(error){
+                callback('Unable to fetch weather.')
+            }
+            else
+            {
+              
+
+                callback('it\'s ' + body.main.temp +' in '+ body.name + '!!');
+            }
+        });
+
+      
+    
+}
+
