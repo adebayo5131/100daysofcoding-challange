ff02b8ee7dba382359b9466412075cf1a8175dcd
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2018-08-03 14:06:34 -0400

    Added GoalList Items'

---
+++ Day 58/goalcoach/src/components/GoalList.jsx
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+
+import {connect} from 'react-redux'
+
+import { goalRef } from '../firebase';
+
+import {setGoals} from '../action'
+
+import GoalItem from './GoalItem'
+
+class GoaList extends Component {
+
+    componentDidMount(){
+        goalRef.on('value', snap => {
+             let goals =[];
+            snap.forEach(goal => {
+                const {email, title } = goal.val();
+                goals.push({email, title})
+            })
+            console.log('goals', goals)
+            this.props.setGoals(goals);
+        })
+    }
+    render(){
+
+        console.log('this props.goals', this.props.goals)
+        return(
+
+            <div>
+             
+            {
+                this.props.goals.map( (goal, index) => {
+
+                    return(
+                       
+                        <GoalItem key={index} goal={goal} />
+                    )
+                })
+                
+                
+            }
+            </div>
+        )
+    }
+    
+}
+
+function mapStateToProps(state){
+    const {goals} = state;
+    return{
+        goals
+    }
+}
+
+
+export default connect (mapStateToProps, {setGoals}) (GoaList);
---
+++ Day 58/goalcoach/src/firebase.js
@@ -0,0 +1,12 @@
+import *as firebase from 'firebase';
+
+const config = {
+    apiKey: "Your apikey goes here",
+    authDomain: "Your domain",
+    databaseURL: "Your database URL",
+    projectId: "Your project name",
+    storageBucket: "",
+    messagingSenderId: "sender ID"
+  };
+
+ export const firebaseApp = firebase.initializeApp(config)
