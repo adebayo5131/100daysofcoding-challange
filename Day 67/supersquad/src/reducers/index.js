b910795cea040daad92bdca84b3e9d5963ad30a0
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2018-08-16 00:01:19 -0400

    Added second reducer with heroes function

---
+++ Day 67/supersquad/src/reducers/index.js
@@ -0,0 +1,11 @@
+import {combineReducers} from 'redux';
+
+import characters from './character_reducers';
+import heroes from './heroes_reducers';
+
+
+const rootReducer = combineReducers({
+    characters,
+    heroes
+})
+export default rootReducer;
