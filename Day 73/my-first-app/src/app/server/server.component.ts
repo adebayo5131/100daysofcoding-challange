965d6be95196a55521a3d4fb086fa69986d2b027
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2018-08-25 15:03:14 -0400

    Creating components and decorators

---
+++ Day 73/my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+/* Creating a component */
+
+/* Import component */
+import {Component} from '@angular/core';
+
+/* Adding a class decorator. By adding a @ sign indicates a decorator */
+@Component({
+      selector: 'app-server',
+      templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
