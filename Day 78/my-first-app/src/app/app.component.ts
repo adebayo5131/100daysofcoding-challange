3ba49691aee4a4491fac3c53765f2d4e602c0084
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2018-09-01 08:36:57 -0400

    using conditional statements

---
+++ Day 78/my-first-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+
+  styles: [`
+         h2{
+           color: green;
+         }
+  `]
+})
+export class AppComponent {
+
+}
