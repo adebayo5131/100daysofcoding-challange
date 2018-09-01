a37e4b9916ab0f3989ed9b81775ea7b01d1f05ac
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2018-08-31 22:47:41 -0400

    username assignment'

---
+++ Day 77/my-first-app/src/app/username/username.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+selector: 'app-username',
+templateUrl: './username.component.html',
+styleUrls: ['./username.component.css']
+})
+export class UsernameComponent implements OnInit {
+
+userName = ' ';
+
+
+
+
+
+
+
+constructor() {
+
+
+}
+
+ngOnInit() {
+}
+
+
+
+
+
+
+}
